b85cde643a7092a9487bd70b2085704286231dc9
sijinxiaotongxue <fzulisijun@163.com>
2024-02-06 20:03:53 +0800

    Add files via upload

---
+++ project_makefile/com/src/test.c
@@ -0,0 +1,7 @@
+#include <test.h>
+
+
+int add(int a,int b)
+{
+	return a+b;
+}
---
+++ project_makefile/main/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "test.h"
+
+
+int main()
+{
+	int sum = add(1,2);
+	printf("111111,%d\n",sum);
+	return 0;
+}
